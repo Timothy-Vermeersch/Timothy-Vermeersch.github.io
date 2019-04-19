a6045e33ae3ed930e45592bebe3eaaca428a5da8
Timothy-Vermeersch <44079761+Timothy-Vermeersch@users.noreply.github.com>
2019-04-19 11:30:51 -0700

    Create script.js

---
+++ SIMP/updater/js/script.js
@@ -0,0 +1,14 @@
+function update(){
+    console.log("Called");
+    var ref = database.ref();
+    console.log(ref);
+    ref.once("value")
+  	.then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+        var test = document.getElementById("test");
+        test.innerHTML = childSnapshot.child("test").val();
+  });
+  	});
+}
+
+window.setInterval(update,100);
