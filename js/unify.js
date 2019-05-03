73f5b44ee081288db05cd42d8a2480570bbb7972
GarrettPeake <gepeake@gmail.com>
2019-05-03 12:11:46 -0700

    All HTML headers and links unified

--- js/unify.js
+++ js/unify.js
@@ -1,11 +1,11 @@
 function insertHeader(){
 	document.getElementById('Topbar').innerHTML = 
-			'<h1 id="title">School Interactive Map Project</h1>'+
+			'<h1 id = "title">School Interactive Map Project</h1>'+
             '<h2 id="sub">Branham AP CS 2019</h2>'+
-            '<div id="tab">'+
-            '   <a href="https://timothy-vermeersch.github.io/info/">Information</a>'+
-            '    <a href="https://timothy-vermeersch.github.io/updater/">Updater</a>'+
-            '    <a href="https://branham.cuhsd.org/apps/events/">Calender</a>'+
+            '<div id = "tab">'+
+            '   <a href = "https://bhs.cloud/info.html/">Information</a>'+
+            '    <a href = "https://bhs.cloud/updater.html">Updater</a>'+
+            '    <a href = "https://branham.cuhsd.org/apps/events/">Calendar</a>'+
             '</div>';
 }
 
