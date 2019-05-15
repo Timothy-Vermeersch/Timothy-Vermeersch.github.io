919d6a10cd6b99172d26ae637ac58c01d5d0a349
GarrettPeake <gepeake@gmail.com>
2019-05-15 10:35:50 -0700

    All pages now run unify

--- js/unify.js
+++ js/unify.js
@@ -3,8 +3,8 @@ function insertHeader(){
 			'<h1 id = "title">School Interactive Map Project</h1>'+
             '<h2 id="sub">Branham AP CS 2019</h2>'+
             '<div id = "tab">'+
-            '   <a href = "https://bhs.cloud/info.html">Information</a>'+
-            '    <a href = "https://bhs.cloud/updater.html">Updater</a>'+
+            '   <a href = "/info.html">Information</a>'+
+            '    <a href = "/updater.html">Updater</a>'+
             '    <a href = "https://branham.cuhsd.org/apps/events/">Calendar</a>'+
             '</div>';
 }
