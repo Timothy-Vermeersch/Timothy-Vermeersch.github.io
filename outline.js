cf6430bf866ef505051b4f982372924e58489c27
Timothy-Vermeersch <44079761+Timothy-Vermeersch@users.noreply.github.com>
2020-07-07 01:25:19 -0700

    Not used to js forgot semicolons>

--- outline.js
+++ outline.js
@@ -3,8 +3,8 @@ var topBar = (
   <h1>Timothy Vermeersch's Github Page</h1>
   <h2>This probably looks like garbage cause I randomly decided to delete the previous code and re write this to learn React</h2>
   </div>
-)
+);
 
 ReactDom.render(
   topBar,document.getElementById('root')
-)
+);
