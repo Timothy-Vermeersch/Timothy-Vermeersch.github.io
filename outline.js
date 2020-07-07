f8ea5245a90fcfa71aa8f590329ae4ca443078d2
Timothy-Vermeersch <44079761+Timothy-Vermeersch@users.noreply.github.com>
2020-07-07 02:57:32 -0700

    Test

--- outline.js
+++ outline.js
@@ -1,6 +1,23 @@
 'use strict';
-element = (
-  <h1>In progress(Obviously not much lol)</h1>
-)
-const domContainer = document.querySelector('#root');
-ReactDOM.render(element, domContainer);
+
+const e = React.createElement;
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'h1',
+      "In Progress"
+    );
+  }
+}
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(Header), domContainer);
