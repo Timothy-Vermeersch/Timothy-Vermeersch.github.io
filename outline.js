85f7adc790b2dc3af757c306b538aa030b9ed4b2
Timothy-Vermeersch <44079761+Timothy-Vermeersch@users.noreply.github.com>
2020-07-07 02:00:48 -0700

    Test

--- outline.js
+++ outline.js
@@ -1,8 +1,2 @@
-var topBar = (
-  <div id = "topBar">
-  <h1>Timothy Vermeersch's Github Page</h1>
-  <h2>This probably looks like garbage cause I randomly decided to delete the previous code and re write this to learn React</h2>
-  </div>
-);
-
+var topBar = (<div id = "topBar"><h1>Timothy Vermeersch's Github Page</h1><h2>This probably looks like garbage cause I randomly decided to delete the previous code and re write this to learn React</h2></div>);
 ReactDom.render(topBar,document.getElementById('root'));
