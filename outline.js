3c0ae63c64ad77664522e7ce47c45d8f976aab12
Timothy-Vermeersch <44079761+Timothy-Vermeersch@users.noreply.github.com>
2020-07-07 03:03:43 -0700

    Test

--- outline.js
+++ outline.js
@@ -1,23 +1,6 @@
 'use strict';
 
-const e = React.createElement;
+const e = <h1>In Progress</h1>
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'h1',
-      "In Progress"
-    );
-  }
-}
 const domContainer = document.querySelector('#root');
-ReactDOM.render(e(Header), domContainer);
+ReactDOM.render(e, domContainer);
