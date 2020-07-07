7e8bcad4a9dad851034a5cd1f27da416cb54bf62
Timothy-Vermeersch <44079761+Timothy-Vermeersch@users.noreply.github.com>
2020-07-07 02:52:29 -0700

    Added Title

--- outline.js
+++ outline.js
@@ -1,24 +1,6 @@
 'use strict';
-
-const e = React.createElement;
-
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
-}
+element = (
+  <h1>In progress(Obviously not much lol)</h1>
+)
 const domContainer = document.querySelector('#root');
 ReactDOM.render(e(LikeButton), domContainer);
