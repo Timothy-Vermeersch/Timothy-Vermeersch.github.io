3a0707a52da8484ceaddcc3b02b30c75cd5dafe8
Timothy-Vermeersch <44079761+Timothy-Vermeersch@users.noreply.github.com>
2020-08-13 17:20:35 -0700

    Updates

---
+++ precache-manifest.d2e1682cda2fc7a4e6fd16b8b3e820d4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d694d8fa8374c13102f6b187f1b5886e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3c7319619cb805a6d177",
+    "url": "./static/css/main.a38d2797.chunk.css"
+  },
+  {
+    "revision": "7dcc05848aaf76c7525f",
+    "url": "./static/js/2.29c6f99f.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "./static/js/2.29c6f99f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c7319619cb805a6d177",
+    "url": "./static/js/main.cfba758e.chunk.js"
+  },
+  {
+    "revision": "9f7ccb997190770e5eab",
+    "url": "./static/js/runtime-main.8f186bad.js"
+  },
+  {
+    "revision": "46467e5b4e901eb37fc5b93cbad25ebc",
+    "url": "./static/media/background.46467e5b.jpg"
+  },
+  {
+    "revision": "dc3bc7e0167906baa5e15a790de67fac",
+    "url": "./static/media/example-one.dc3bc7e0.jpg"
+  },
+  {
+    "revision": "2c8f6ba71925e87cec78bb0d59fe3c15",
+    "url": "./static/media/example-three.2c8f6ba7.jpg"
+  },
+  {
+    "revision": "948bd2a464571b652398491b5e3c0d2c",
+    "url": "./static/media/example-two.948bd2a4.jpg"
+  },
+  {
+    "revision": "45f44ae67cdbb242c3aa480a4d4615d6",
+    "url": "./static/media/githubPages.45f44ae6.png"
+  }
+]);
