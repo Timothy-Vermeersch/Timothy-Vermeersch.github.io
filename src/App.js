7d3325b1cd53eee7bbae8f8af10a6707ab8f7a0d
Timothy-Vermeersch <44079761+Timothy-Vermeersch@users.noreply.github.com>
2020-08-25 16:00:33 -0700

    Tweaked page routing

--- src/App.js
+++ src/App.js
@@ -5,18 +5,23 @@ import Home from './Home';
 import ProjectList from './Projects/ProjectList';
 import GameContainer from './Games/GameContainer';
 import GamesList from './Games/GamesList'
-import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
+import About from './About/About'
+import { Link, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ProjectRouter from './Projects/ProjectRouter';
 
 function App() {
   return (
     <Router>
       <PageFrame />
-      <Route exact path = "/" component = {Home} />
-      <Route exact path = "/Projects" component = {ProjectList}/>
-      <Route exact path = "/Projects/:projectId" component = {ProjectRouter}/>
-      <Route exact path = "/Games" component = {GamesList}/>
-      <Route exact path = "/Games/:gameId" component = {GameContainer}/>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Projects" component={ProjectList} />
+        <Route exact path="/Projects/:projectId" component={ProjectRouter} />
+        <Route exact path="/Games" component={GamesList} />
+        <Route exact path="/Games/:gameId" component={GameContainer} />
+        <Route exact path="/About" component={About} />
+        <Route component={Home}/>
+      </Switch>
     </Router>
   );
 }
--- src/Games/GameContainer.js
+++ src/Games/GameContainer.js
@@ -6,7 +6,14 @@ import Flappy from './Flappy/Flappy.js';
 const gamesDic = {"BlackJack":<BlackJack/>, "TicTacToe":<TicTacToe/>, "Flappy":<Flappy/>}
 
 const GameContainer = ({match, location}) =>{
-    return gamesDic[match.params.gameId]
+    if(Object.keys(gamesDic).includes(match.params.gameId)){
+        return gamesDic[match.params.gameId];
+    }
+    return (
+        <div id = "No-Game">
+            <h1>Could not find game with name {match.params.gameId}</h1>
+        </div>
+    );
 }
 
 export default GameContainer;
