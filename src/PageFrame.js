cca3ede33b9d8f8d6bc2c2f010fe2a5ed597fb42
Timothy-Vermeersch <44079761+Timothy-Vermeersch@users.noreply.github.com>
2020-08-25 16:02:23 -0700

    Tweaked routing

--- src/PageFrame.js
+++ src/PageFrame.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import pagesLogo from './Images/githubPages.png'
-import { Link, HashRouter as Router, Route } from 'react-router-dom';
+import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
 
 function PageFrame(){
     return (
