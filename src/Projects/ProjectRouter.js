378c29a034415194c8cdd817d238d4599713a5ab
Timothy-Vermeersch <44079761+Timothy-Vermeersch@users.noreply.github.com>
2020-08-27 16:26:25 -0700

    Added message board project

--- src/Projects/ProjectRouter.js
+++ src/Projects/ProjectRouter.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Bot from './Bot/Bot';
 import './Project.css';
+import MessageBoard from './MessageBoard/MessageBoard'
 
-var projectDictionary = {"Bot":<Bot/>}
+var projectDictionary = {"Bot":<Bot/>, "MessageBoard": <MessageBoard/>};
 const GameContainer = ({match, location}) =>{
     return projectDictionary[match.params.projectId];
 }
